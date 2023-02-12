0af303cc0d05f1b588a608dfabe0154c29c1777e
officialsnowwisdom <officialsnowwisdom@gmail.com>
2023-02-12 22:16:49 +0100

    Qr Generator Complete Code commit

--- qrcodegenerator.py
+++ qrcodegenerator.py
@@ -7,9 +7,9 @@ qr = qrcode.QRCode(
     border= "4"
 )
 
-data = "https://snowwisdom.com"
+data = "https://snowwisdom.com" # YOu can change the link here to what ever you want
 
 qr.add_data(data)
 qr.make(fit=True)
-img = qr.make_image(fill_color="green", back_color="white")
-img.save("code.png")
+img = qr.make_image(fill_color="green", back_color="white") # set the look here
+img.save("code.png") # saving the image to this file name
